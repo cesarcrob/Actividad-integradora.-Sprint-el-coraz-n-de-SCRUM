547c67c5273b8de19fe3237dfce221af3bcc6ef2
cesarcrob <124860632+cesarcrob@users.noreply.github.com>
2023-02-09 21:22:30 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,151 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+   <h1 className="title">
+   SPRINT PLANNING
+   </h1>
+   <h2>Fichas Backlog</h2>
+<p>
+<strong>Historia de Usuario 1:</strong> AD HU 01 <br></br>
+Como administrador quiero registrar los datos de los clientes para tener una base de datos. <br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 1.1:</strong> Diseñar la estructura y el modelo de gestionar clientes.<br></br>
+<strong>Tarea 1.2:</strong> Programar la estructura diseñada.<br></br>
+<strong>Tarea 1.3:</strong> Realizar prueba de registro de clientes.<br></br><br></br>
+
+<strong>Historia de Usuario 2:</strong> AD HU 02<br></br>
+Como administrador quiero actualizar los datos de los clientes para tener una base de datos actualizada.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 2.1:</strong> Diseñar método editar y modificar.<br></br>
+<strong>Tarea 2.2:</strong> Programar el diseño editar y agregar botones.<br></br>
+<strong>Tarea 2.3:</strong> Realizar prueba de actualización de datos.<br></br><br></br>
+
+<strong>Historia de Usuario 3:</strong> AD HU 03<br></br>
+Como Administrador quiero consultar los datos de los clientes otorgándoles autoridad para realizar una toma de decisiones.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 3.1:</strong> Diseñar el formulario de consulta de clientes.<br></br>
+<strong>Tarea 3.2:</strong> Programar el formulario de consulta de clientes.<br></br>
+<strong>Tarea 3.3:</strong> Realizar prueba de consulta de clientes.<br></br><br></br>
+
+<strong>Historia de Usuario 4:</strong> EC HU 04<br></br>
+Como Ejecutivo de Cuenta quiero alimentar el sistema de almacenamiento con los proyectos correspondientes del cliente.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 4.1:</strong> Diseñar el módulo de gestionar los proyectos de los clientes.<br></br>
+<strong>Tarea 4.2:</strong> Programar el módulo de gestión de proyectos de clientes.<br></br>
+<strong>Tarea 4.3:</strong> Realizar prueba de control de gestión de proyectos de clientes.<br></br><br></br>
+
+<strong>Historia de Usuario 5:</strong> EC HU 05<br></br>
+Como Ejecutivo de Cuenta quiero modificar los proyectos almacenados de un cliente en caso que exista alguna modificación para llevar el control.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 5.1:</strong> Diseñar el formulario de modificar proyectos (suplantar archivos).<br></br>
+<strong>Tarea 5.2:</strong> Programar el método modificar proyecto.<br></br>
+<strong>Tarea 5.3:</strong> Realizar prueba del método modificar proyecto.<br></br><br></br>
+
+<strong>Historia de Usuario 6:</strong> AD HU 06<br></br>
+Como Administrador quiero eliminar o modificar los archivos almacenados de los clientes según lo requerido.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 6.1:</strong> Diseñar el formulario de eliminar o modificar archivos almacenados.<br></br>
+<strong>Tarea 6.2:</strong> Programar el método eliminar o modificar archivos almacenados.<br></br>
+<strong>Tarea 6.3:</strong> Realizar prueba de eliminar o modificar archivos almacenados.<br></br><br></br>
+
+<h2>Calendarización de los Sprint</h2>
+
+<h3><strong>SPRIN 1</strong><br></br></h3>
+
+<strong>Historia de Usuario 1:</strong> AD HU 01<br></br>
+Como administrador quiero registrar los datos de los clientes para tener una base de datos.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 1.1:</strong> Diseñar la estructura y el modelo de gestionar clientes. <strong>| Estimado:</strong> 5 días<br></br>
+<strong>Tarea 1.2:</strong> Programar la estructura diseñada. <strong>| Estimado:</strong> 3 días<br></br>
+<strong>Tarea 1.3:</strong> Realizar prueba de registro de clientes. <strong>| Estimado:</strong> 2 días<br></br>
+<strong>Semana:</strong> 1<br></br>
+<strong>Prioridad:</strong> Alta<br></br><br></br>
+
+<strong>Historia de Usuario 2:</strong> AD HU 02<br></br>
+Como administrador quiero actualizar los datos de los clientes para tener una base de datos actualizada.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 2.1:</strong> Diseñar método editar y modificar. <strong>| Estimado:</strong> 5 días<br></br>
+<strong>Tarea 2.2:</strong> Programar el diseño editar y agregar botones. <strong>| Estimado:</strong> 3 días<br></br>
+<strong>Tarea 2.3:</strong> Realizar prueba de actualización de datos. <strong>| Estimado:</strong> 2 días<br></br>
+<strong>Semana:</strong> 2<br></br>
+<strong>Prioridad:</strong> Alta<br></br><br></br>
+
+<h3><strong>SPRIN 2</strong><br></br></h3>
+
+<strong>Historia de Usuario 3:</strong> AD HU 03<br></br>
+Como Administrador quiero consultar los datos de los clientes otorgándoles autoridad para realizar una toma de decisiones.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 3.1:</strong> Diseñar el formulario de consulta de clientes.  <strong>| Estimado:</strong> 5 días<br></br>
+<strong>Tarea 3.2:</strong> Programar el formulario de consulta de clientes. <strong>| Estimado:</strong> 3 días<br></br>
+<strong>Tarea 3.3:</strong> Realizar prueba de consulta de clientes. <strong>| Estimado:</strong> 2 días<br></br>
+<strong>Semana:</strong> 3<br></br>
+<strong>Prioridad:</strong> Alta<br></br><br></br>
+
+<strong>Historia de Usuario 4:</strong> EC HU 04<br></br>
+Como Ejecutivo de Cuenta quiero alimentar el sistema de almacenamiento con los proyectos correspondientes del cliente.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 4.1:</strong> Diseñar el módulo de gestionar los proyectos de los clientes. <strong>| Estimado:</strong> 5 días<br></br>
+<strong>Tarea 4.2:</strong> Programar el módulo de gestión de proyectos de clientes. <strong>| Estimado:</strong> 3 días<br></br>
+<strong>Tarea 4.3:</strong> Realizar prueba de control de gestión de proyectos de clientes. <strong>| Estimado:</strong> 2 días<br></br>
+<strong>Semana:</strong> 4<br></br>
+<strong>Prioridad:</strong> Alta<br></br><br></br>
+
+<h3><strong>SPRIN 3</strong><br></br></h3>
+
+<strong>Historia de Usuario 5:</strong> EC HU 05<br></br>
+Como Ejecutivo de Cuenta quiero modificar los proyectos almacenados de un cliente en caso que exista alguna modificación para llevar el control.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 5.1:</strong> Diseñar el formulario de modificar proyectos (suplantar archivos). <strong>| Estimado:</strong> 3 días<br></br>
+<strong>Tarea 5.2:</strong> Programar el método modificar proyecto. <strong>| Estimado:</strong> 2 días<br></br>
+<strong>Tarea 5.3:</strong> Realizar prueba del método modificar proyecto. <strong>| Estimado:</strong> 1 días<br></br>
+<strong>Semana:</strong> 5<br></br>
+<strong>Prioridad:</strong> Baja<br></br><br></br>
+
+<strong>Historia de Usuario 6:</strong> AD HU 06<br></br>
+Como Administrador quiero eliminar o modificar los archivos almacenados de los clientes según lo requerido.<br></br>
+<strong>Tareas:</strong><br></br>
+<strong>Tarea 6.1:</strong> Diseñar el formulario de eliminar o modificar archivos almacenados. <strong>| Estimado:</strong> 3 días<br></br>
+<strong>Tarea 6.2:</strong> Programar el método eliminar o modificar archivos almacenados. <strong>| Estimado:</strong> 2 días<br></br>
+<strong>Tarea 6.3:</strong> Realizar prueba de eliminar o modificar archivos almacenados. <strong>| Estimado:</strong> 1 días<br></br>
+<strong>Semana:</strong> 6<br></br>
+<strong>Prioridad:</strong> Baja<br></br><br></br>
+   </p>
+   <input type="text" className="new-task"/>
+</div>
+  );
+}
+
+export default App;
+
+
+
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
